4bd19ca70a1a890f98cf7811fa7c88eb92277fb3
Baranovsky Maxim <baranowski.maxim@gmail.com>
2021-06-17 14:37:55 +0300

    feat: complete task 3

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,25 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import javax.swing.text.DateFormatter
 import kotlin.reflect.KClass
+import kotlin.reflect.typeOf
 
 class Blocks {
-
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        when(blockB) {
+            String::class -> {
+                return blockA.filterIsInstance<String>().joinToString(separator = "")
+            }
+            Int::class -> {
+                return blockA.filterIsInstance<Int>().sum()
+            }
+            LocalDate::class -> {
+                return blockA.filterIsInstance<LocalDate>().max()?.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))!!
+            }
+        }
+
+        return "Empty"
     }
 }
